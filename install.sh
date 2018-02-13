fc13882817ef7ec0645d2048a403ca66a83ef70c
Michaël Roy <microygh@gmail.com>
2018-02-14 00:10:12 +0100

    Add a package to enable IPv6 temporary addresses.

--- install.sh
+++ install.sh
@@ -2,14 +2,15 @@
 
 # Select the different configurations to install
 SELECT=$(whiptail --title "mdebian-base" --checklist --separate-output \
-"Choose custom configurations :" 20 78 7 \
+"Choose custom configurations :" 20 78 8 \
 "apt" "Configure apt" ON \
-"banner" "Configure banners" ON \
+"banner" "Configure system banners" ON \
 "bash" "Configure bash" ON \
 "grub" "Add GRUB configuration scripts" ON \
+"ipv6" "Enable IPv6 temporary addresses" ON \
 "minicom" "Configure minicom" OFF \
 "nano" "Configure nano" ON \
-"ntp" "Configure ntp" ON \
+"ntp" "Configure ntp client" ON \
 3>&1 1>&2 2>&3)
 
 # No option selected or canceled
